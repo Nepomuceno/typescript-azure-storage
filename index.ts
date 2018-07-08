479fb6b139128e62528e9a8dc2faa95452aaf0b7
Gabriel Nepomuceno <ganepmu@microsoft.com>
2018-07-08 19:39:38 +0100

    Step 3 - insert new entities

--- index.ts
+++ index.ts
@@ -3,6 +3,12 @@ import { Storage } from "./storage";
 const tableName = "sampletable";
 
 (async () => {
-  await Storage.Create(tableName);
+  const storage = await Storage.Create(tableName);
+  await storage.AddOrMergeRecord({
+    PartitionKey: "SamplePartition",
+    RowKey: "SampleRow",
+    SampleInt: 42,
+    SampleString: "Amazing!"
+  });
   console.log("Table created");
 })();
--- storage.ts
+++ storage.ts
@@ -1,28 +1,69 @@
-import * as storage from "azure-storage"
+import * as storage from "azure-storage";
 import * as dotenv from "dotenv";
+import { resolve } from "url";
 
 dotenv.load();
 
-export class Storage{
-    private tableService : storage.TableService;
-    private tableName: string = "default";
-    private constructor() {
-        this.tableService = storage.createTableService();
-    }
-    static async Create(tableName: string) : Promise<Storage> {
-        var me = new Storage()
-        me.tableName = tableName;
-        await me.CreateIfDoesntExistTable();
-        return me;
-    }
-    private async CreateIfDoesntExistTable() : Promise<storage.TableService.TableResult> {
-        return new Promise((resolve, reject) =>{
-            try {
-                this.tableService.createTableIfNotExists(this.tableName, (err,result) => {
-                    if(err) throw err;
-                    resolve(result);
-                });
-            } catch (err) { reject(err); }
-        })
-    }
+export class Storage {
+  private tableService: storage.TableService;
+  private tableName: string = "default";
+  private constructor() {
+    this.tableService = storage.createTableService();
+  }
+  static async Create(tableName: string): Promise<Storage> {
+    var me = new Storage();
+    me.tableName = tableName;
+    await me.CreateIfDoesntExistTable();
+    return me;
+  }
+  private async CreateIfDoesntExistTable(): Promise<
+    storage.TableService.TableResult
+  > {
+    return new Promise((resolve, reject) => {
+      try {
+        this.tableService.createTableIfNotExists(
+          this.tableName,
+          (err, result) => {
+            if (err) throw err;
+            resolve(result);
+          }
+        );
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  async AddOrMergeRecord(record: ITableEntity): Promise<ITableEntity> {
+    return new Promise<ITableEntity>((resolve, reject) => {
+      try {
+        const tr = this.convertToTableRecord(record);
+        this.tableService.insertOrMergeEntity(this.tableName, tr, err => {
+          if (err) throw err;
+          resolve(record);
+        });
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+
+  private convertToTableRecord(entity: ITableEntity) {
+    let result: any = {};
+    Object.keys(entity).forEach(k => {
+      let prop = Object.getOwnPropertyDescriptor(entity, k);
+      if (prop) {
+        result[k] = new storage.TableUtilities.entityGenerator.EntityProperty(
+          prop.value
+        );
+      }
+    });
+    return result;
+  }
+}
+
+export interface ITableEntity {
+  PartitionKey: string;
+  RowKey: string;
+  [key: string]: string | number | boolean | undefined;
 }
